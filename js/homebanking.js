d293f06ffa17bef47d44057233f9afc736739882
Camilo Dominguez <camiloa17@gmail.com>
2019-03-21 19:24:56 -0300

    fix document structure

--- js/homebanking.js
+++ js/homebanking.js
@@ -4,10 +4,10 @@ var saldoCuenta = 0;
 var limiteExtraccion = 0;
 
 var camiloDominguez = {
-    nombreUsuario:"Camilo Dominguez",
-    saldoCuenta:3400,
+    nombreUsuario: "Camilo Dominguez",
+    saldoCuenta: 3400,
     limiteExtraccion: 1000,
-}
+};
 
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
@@ -16,7 +16,7 @@ window.onload = function () {
     actualizarSaldoEnPantalla();
     actualizarLimiteEnPantalla();
     iniciarSesion();
-}
+};
 
 
 //Funciones que tenes que completar
@@ -27,9 +27,9 @@ function cambiarLimiteDeExtraccion() {
         alert("No ingresaste ningun monto tu Limite actual es " + "$" + limiteExtraccion);
 
     } else if (limiteNuevo) {
-        calc(parseFloat(limiteNuevo),"lim","Limite de antes $")
-    }
-}
+        calc(parseFloat(limiteNuevo), "lim", "Limite de antes $")
+    };
+};
 
 /*function que trae el dato del cliente y mira si es correcto para pasarlo a la funcion calc. */
 function extraerDinero() {
@@ -39,8 +39,8 @@ function extraerDinero() {
         alert("No ingresaste ningun monto tu Saldo es " + "$" + saldoCuenta);
     } else if (saldoExtraer) {
         calc(parseFloat(saldoExtraer), "-", "Extrajiste: ");
-    }
-}
+    };
+};
 /*function que trae el dato del cliente y mira si es correcto para pasarlo a la funcion calc. */
 
 function depositarDinero() {
@@ -51,33 +51,33 @@ function depositarDinero() {
         alert("No ingresaste ningun monto tu Saldo es " + "$" + saldoCuenta);
     } else if (deposit) {
         calc(parseFloat(deposit), "+", "Depositaste:");
-    }
-}
+    };
+};
 /*function que trae el dato del cliente y mira si si correcto, luego mira que tipo de servicio es y lo pasa a calc. */
 
 function pagarServicio() {
     var pagarServicio = prompt("Ingresa el numero del servicio que queres pagar:" + "\n" + "1 - Agua" + "\n" + "2 - Luz" + "\n" + "3 - Internet" + "\n" + "4 - Telefono");
 
-    if(pagarServicio === ""){
+    if (pagarServicio === "") {
         alert("No ingresaste ningun servicio tu Saldo es " + "$" + saldoCuenta);
-    }else {
-    switch (pagarServicio) { 
-        case "1":
-            calc(400, "-", "Pagaste por el servicio de Agua:");
-            break;
+    } else {
+        switch (pagarServicio) {
+            case "1":
+                calc(400, "-", "Pagaste por el servicio de Agua:");
+                break;
 
-        case "2":
-            calc(600, "-", "Pagaste por el servicio de Luz:");
-            break;
-        case "3":
-            calc(800, "-", "Pagaste por el servicio de Intenet:");
-            break;
-        case "4":
-            calc(950, "-", "Pagaste por el servicio de Telefono:");
-            break;
-    }
-    }
-}
+            case "2":
+                calc(600, "-", "Pagaste por el servicio de Luz:");
+                break;
+            case "3":
+                calc(800, "-", "Pagaste por el servicio de Intenet:");
+                break;
+            case "4":
+                calc(950, "-", "Pagaste por el servicio de Telefono:");
+                break;
+        };
+    };
+};
 
 /*function que trae el dato del cliente y mira si es correcto para pasarlo a la funcion calc. */
 function transferirDinero() {
@@ -90,9 +90,9 @@ function transferirDinero() {
     } else if (transferir) {
         calc(parseFloat(transferir), "-", "Transferiste:");
 
-    }
+    };
 
-}
+};
 
 /* Calcula de acuerdo a tres inputs que vienen de las funciones del tipo de operacion a realizar */
 function calc(value, symbol, typeOfMovement) {
@@ -108,20 +108,20 @@ function calc(value, symbol, typeOfMovement) {
             alert(typeOfMovement + "$" + newValue + "\n" + "Saldo Anterior $" + oldValue + "\n" + "Saldo actual: $" + saldoCuenta);
         } else if (newBalance < 0) {
             alert("No tienes fondos Suficientes para hacer la operacion");
-        }
+        };
 
     } else if (symbol === "+") {
         saldoCuenta = oldValue + newValue;
         actualizarSaldoEnPantalla();
         alert(typeOfMovement + " $" + newValue + "\n" + "Saldo Anterior $" + oldValue + "\n" + "Saldo actual: $" + saldoCuenta);
 
-    }else if(symbol ==="lim"){
+    } else if (symbol === "lim") {
         limiteExtraccion = value;
         actualizarLimiteEnPantalla();
         alert(typeOfMovement + oldLimit + "\n" + "Limite Nuevo $ " + limiteExtraccion);
-    }
+    };
 
-}
+};
 
 /*Funcion que mira si las datos ingresados son los correcto y llama al objeto de camiloDominguez para agregar sus datos */
 function iniciarSesion() {
@@ -134,7 +134,7 @@ function iniciarSesion() {
         alert("Ingresaste un codigo erroneo, intenta de nuevo");
         setTimeout(function () {
             iniciarSesion();
-        }, 400); 
+        }, 400);
 
     } else {
         alert("Bienvenido Camilo Dominguez!" + "\n" + "Ya puedes utilizar tu Home Banking");
@@ -144,22 +144,22 @@ function iniciarSesion() {
         actualizarSaldoEnPantalla();
         cargarNombreEnPantalla();
         actualizarLimiteEnPantalla();
-    }
-}
+    };
+};
 
 
 //Funciones que actualizan el valor de las variables en el HTML
 function cargarNombreEnPantalla() {
     document.getElementById("nombre").innerHTML = "Bienvenido/a " + nombreUsuario;
-}
+};
 
 function actualizarSaldoEnPantalla() {
     document.getElementById("saldo-cuenta").innerHTML = "$" + saldoCuenta;
-}
+};
 
 function actualizarLimiteEnPantalla() {
     document.getElementById("limite-extraccion").innerHTML = "Tu límite de extracción es: $" + limiteExtraccion;
-}
+};
 
 
 /*add event listener to all buttons*/
@@ -192,5 +192,5 @@ for (i = 0; i < count; i++) {
         }
 
     });
-}
+};
 
